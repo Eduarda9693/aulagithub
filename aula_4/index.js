1a59106f776df7f54fefecdfdd5f4707492e094f
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-10 11:53:55 -0300

    testando conversão string/number

--- aula_4/index.js
+++ aula_4/index.js
@@ -1,1 +1,9 @@
 console.log("Hello World")
+const nome = "Eduarda"
+const idade = 15
+console.log("Meu nome é ",nome," e tenho ",idade, " anos de idade")
+// teste
+const  idadeNumero = 23
+const idadeTexto = idadeNumero.toString()
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)
