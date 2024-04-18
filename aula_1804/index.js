0631c623b624512794d7d819466cf1e969e2cc99
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-18 12:01:32 -0300

    string e arrays

--- aula_1804/index.js
+++ aula_1804/index.js
@@ -4,11 +4,18 @@ let cor = prompt("Qual a sua cor favorita?")
 alert(`a cor favorita de ${nome} é ${cor}`)*/
 
 /*let frase = prompt("Digite uma frase: ")
-console.log(frase.touUpperCase())
-console.log(frase.replace11("o","i"))
+console.log(frase.toUpperCase())
+console.log(frase.replacell("o","i"))
 console.log(frase.length)
-console.log(frase.replace11("o","i").toUpperCase().replace11("H","P"))*/
+console.log(frase.replacell("o","i").toUpperCase().replacell("H","P"))*/
 
-let racaCachorro = ("Pittbull", "caneCorso", "ViraLata")
-let optionRaca = prompt("Digite um num de 0 a 3")
-alert(racaCachorro{optionRaca})
+/*let racaCachorro = ("Pittbull", "caneCorso", "ViraLata", "Pincher", "York")
+let optionRaca = prompt("Digite um número de 0 a 4")
+alert(racaCachorro{optionRaca})*/
+
+let number = [1,2,3,4,5,6]
+console.log(numbers.length)
+number.push(7)
+numbers.splice(3,2)
+console.log(numbers.length)
+console.log(numbers)
