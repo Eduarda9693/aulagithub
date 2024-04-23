76ce5e4bca3c8bae94c1d23052096303e6d73b14
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-23 11:57:36 -0300

    Exercícios 23.04

---
+++ aula_2304/index.js
@@ -0,0 +1,59 @@
+// exercício 1
+// let num1 = prompt("Digite um número: ")
+// let num2 = prompt("Digite outro número: ")
+// if (num1 === num2){
+    // alert("Esses números são iguais") // IMPRIME NO CHROOME
+    //console.log("Esses números são iguais") //IMPRIME NO CONSOLE
+// } else {
+    // alert("Esses números não são iguais")
+    //console.log("Esses números não são iguais")
+//}
+
+//exercício 2 e 3
+/*let num1 = prompt("Digite o primeiro número: ")
+let num2 = prompt("Digite o segundo número: ")
+if (num1 === num2){
+    alert("números iguais")
+} else if(num1 > num2){
+    alert(num1 + " é maior que " + num2)
+} else if(num1 < num2){
+    alert(num1 + " é menor que " + num2)
+}*/
+
+// exercício 4
+/*let pokemon = prompt("Digite seu primeiro pokemon: ")
+switch (pokemon){
+    case 'Balbussauro':
+        alert(`planta e veneno!!!`)
+        break
+    case 'Charmander':
+        alert(`fogo!!!`)
+        break
+    case 'Squirtle':
+        alert(`água!!!`)
+        break
+default:
+    alert(`Não é um pokemon inicial!!!`)
+    break
+}*/
+        // exercício 5
+
+/*let idade = Number(prompt("Qual é a sua idade"))
+let ensinoMedio = prompt("Concluiu o ensino médio? sim - não")
+let faculdade = prompt("Está na faculdade? sim - não")
+if ((idade >= 18) && (ensinoMedio == "sim") && (faculdade == "não")){
+    alert("Você está apto para entrar na faculdade")
+    } else{
+        alert("Você não está apto para a faculadade")
+        }*/
+
+    
+//Exercícios notion escrita de código
+ let numero = Number(prompt("Digite sua idade: "))
+ if(numero >= 18){
+    alert("Você pode dirigir")
+ } else{
+    alert("Você não pode dirigir")
+ }
+
+
