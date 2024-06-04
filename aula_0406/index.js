84e2831f097003f4a9e2b2d6ff84688c5b40291d
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-06-04 11:53:53 -0300

    Exercícios de objetos p2

---
+++ aula_0406/index.js
@@ -0,0 +1,5 @@
+const pessoa = {
+	nome: "Eduarda",
+	apelidos: ["Duda", "Du", "Duds"],
+}
+console.log("Eu sou", pessoa.nome,"," ,"mas pode me chamar de:", pessoa.apelidos[0], ",", pessoa.apelidos[1], ",", pessoa.apelidos[2])
